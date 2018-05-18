337942f93b1d1cc0a0611e700f07673c7dd70d35
freedmand <freedmand@gmail.com>
2018-05-18 15:06:00 -0700

    Update responsive-virtual-list code

--- src/responsive-virtual-list/responsive-virtual-list.js
+++ src/responsive-virtual-list/responsive-virtual-list.js
@@ -1,42 +1,120 @@
+const SCROLL_OUT_OF_BOUNDS = 'The scroll position is out of bounds';
+
 export class Viewport {
   constructor(height, pages, initialPage = 0, scrollTop = 0) {
     this.height = height;
     this.scrollTop = scrollTop;
     /** @type {!PageSet} */
-    this.pageSet = new PageSet(this, pages);
+    this.pageSet = new PageSet(this, pages, initialPage);
   }
 
-  jump(pageNum) {
-    return this.jumpIndex(pageNum - 1);
+  updateHeight(height) {
+    this.height = height;
+    this.pageSet.update();
   }
 
-  jumpIndex(pageIndex) {
-    this.scrollTo(this.pageSet.positionOf(pageIndex));
+  /**
+   * Scroll to the top of the specified page, or the bottom of the entire page
+   * set if needed.
+   * @param {number} pageIndex
+   * @param {boolean} update Whether to update the page set after scrolling.
+   */
+  scrollToTopOfPage(pageIndex, update = true) {
+    this.scrollToPosition(Math.min(
+        this.pageSet.positionOfPageTop(pageIndex),
+        this.maxScrollPosition()), update);
   }
 
-  scrollTo(position) {
+  /**
+   * Scroll to the bottom of the specified page, or the bottom of the entire
+   * page set if needed.
+   * @param {number} pageIndex
+   * @param {boolean} update Whether to update the page set after scrolling.
+   */
+  scrollToBottomOfPage(pageIndex, update = true) {
+    this.scrollToPosition(Math.min(
+      this.pageSet.positionOfPageBottom(pageIndex),
+      this.maxScrollPosition()), update);
+  }
+
+  /**
+   * Scroll by the specified offset relative to the current scroll position.
+   * @param {number} delta The delta to scroll by.
+   * @param {boolean} update Whether to update the page set after scrolling.
+   */
+  scrollDelta(delta, update = true) {
+    this.scrollToPosition(this.scrollTop + delta, update);
+  }
+
+  /**
+   * Scroll to the specified percentage.
+   * @param {number} percentage
+   * @param {boolean} update Whether to update the page set after scrolling.
+   */
+  scrollToPercentage(percentage, update) {
+    this.scrollToPosition(percentage * this.maxScrollPosition(), update);
+  }
+
+  /**
+   * Scroll to the specified position.
+   * @param {number} position
+   * @param {boolean} update Whether to update the page set after scrolling.
+   * @throws {Error} The scroll position is out of bounds.
+   */
+  scrollToPosition(position, update = true) {
+    // Instead of throwing an error, just clamp the scroll so that it is "safe".
+    if (position < 0) position = 0;
+    const maxScrollPosition = this.maxScrollPosition();
+    if (position > maxScrollPosition) position = maxScrollPosition;
+
+    // if (position < 0 || position > this.maxScrollPosition()) {
+    //   throw new Error(SCROLL_OUT_OF_BOUNDS);
+    // }
     this.scrollTop = position;
+    if (update) this.pageSet.update();
   }
 
+  /**
+   * Return the maximum possible position for the scroll bar.
+   * @return {number}
+   */
+  maxScrollPosition() {
+    return this.pageSet.totalHeight - this.height;
+  }
+
+  /**
+   * Return the percentage of the scroll bar, where 0 is at the top and 1.0 is
+   * at the bottom.
+   * @return {number}
+   */
+  scrollPercentage() {
+    return this.scrollTop / this.maxScrollPosition();
+  }
+
+  /**
+   * Return the absolute position of the top of the viewport in the page set.
+   * @return {number}
+   */
   top() {
     return this.scrollTop;
   }
 
+  /**
+   * Return the absolute position of the bottom of the viewport in the page set.
+   * @return {number}
+   */
   bottom() {
     return this.scrollTop + this.height;
-  }
-
-  load() {
-
   }
 }
 
 export class PageSet {
   /**
-   * @param {!Viewport} viewport 
-   * @param {!Array<Page>} pages 
+   * @param {!Viewport} viewport
+   * @param {!Array<Page>} pages
+   * @param {number} initialPage The initial page visible.
    */
-  constructor(viewport, pages) {
+  constructor(viewport, pages, initialPage) {
     /** @type {!Array<Page>} */
     this.pages = pages;
     /** @type {!Viewport} */
@@ -44,7 +122,7 @@ export class PageSet {
     this.heightMap = [];
     this.totalHeight = 0;
     this.observed = new Set(); // which pages have been observed
-    this.init();
+    this.init(initialPage);
   }
 
   estimatedPageHeight() {
@@ -66,8 +144,12 @@ export class PageSet {
 
   visible() {
     const [top, bottom] = [this.viewport.top(), this.viewport.bottom()];
-    console.log([top, bottom]);
-    return this.range(this.pageAtPosition(top), this.pageAtPosition(bottom));
+    return this.range(this.pageAtPosition(top), this.pageAtPosition(bottom, true));
+  }
+
+  update() {
+    const visiblePages = this.visible();
+    visiblePages.forEach((page) => page.updateHeight());
   }
 
   range(page1, page2) {
@@ -80,7 +162,7 @@ export class PageSet {
     return pages;
   }
 
-  positionOf(index) {
+  positionOfPageTop(index) {
     let accumulatedHeight = 0;
     for (let i = 0; i < index; i++) {
       accumulatedHeight += this.heightMap[i];
@@ -88,21 +170,39 @@ export class PageSet {
     return accumulatedHeight;
   }
 
+  positionOfPageBottom(index) {
+    let accumulatedHeight = 0;
+    for (let i = 0; i <= index; i++) {
+      accumulatedHeight += this.heightMap[i];
+    }
+    return accumulatedHeight;
+  }
+
   /**
-   * @param {number} position 
+   * @param {number} position
+   * @param {boolean} backwards Whether to search from a bottom position.
    * @return {!Page}
    */
-  pageAtPosition(position) {
+  pageAtPosition(position, backwards = false) {
     let offset = 0;
     for (let i = 0; i < this.pages.length; i++) {
       let pageBottom = offset + this.heightMap[i];
-      if (position >= offset && position < pageBottom) return this.pages[i];
+      if (!backwards) {
+        if (position >= offset && position < pageBottom) {
+          return this.pages[i];
+        }
+      } else {
+        if (position > offset && position <= pageBottom) {
+          return this.pages[i];
+        }
+      }
+
       offset = pageBottom;
     }
     throw new Error('Position outside of doc');
   }
 
-  updateHeights() {
+  updateEstimatedHeights() {
     const estimate = this.estimatedPageHeight();
     let total = 0;
     for (let i = 0; i < this.pages.length; i++) {
@@ -117,12 +217,19 @@ export class PageSet {
     this.totalHeight = total;
   }
 
-  init() {
+  init(initialPage = 0) {
     let offset = 0;
     let i;
 
+    // Initialize pages before initial page
+    for (i = 0; i < initialPage; i++) {
+      const page = this.pages[i];
+      page.index = i;
+      page.pageSet = this;
+    }
+
     // Initialize initial heightmap until viewport is full.
-    for (i = 0; i < this.pages.length && offset <= this.viewport.height; i++) {
+    for (i = initialPage; i < this.pages.length && offset <= this.viewport.height; i++) {
       const page = this.pages[i];
 
       // Set page properties to keep track of index and percolate events.
@@ -141,13 +248,30 @@ export class PageSet {
       page.pageSet = this;
     }
 
-    this.updateHeights();
+    this.updateEstimatedHeights();
+  }
+
+  /**
+   * Update the scroll position as a new page is being loaded. This should only
+   * cause an effect if the page is before the scroll position.
+   * @param {!Page} page The page that is being loaded that the scroll should be
+   *     recalculated on.
+   */
+  updateScroll(page) {
+    const topPage = this.pageAtPosition(this.viewport.top());
+    if (page.index < topPage.index) {
+      // Only if the page being loaded is before the first one displayed in the
+      // viewport does the scroll need to be shifted.
+      const delta = page.height - this.heightMap[page.index];
+      this.viewport.scrollDelta(delta, false);
+    }
   }
 
   pageUpdated(page) {
+    this.updateScroll(page);
     this.heightMap[page.index] = page.height;
     this.observed.add(page.index);
-    this.updateHeights();
+    this.updateEstimatedHeights();
   }
 }
 
@@ -163,8 +287,8 @@ export class Page {
     this.pageSet.pageUpdated(this);
   }
 
-  updateHeight(height) {
-    this.height = height;
+  updateHeight(height = null) {
+    if (height != null) this.height = height;
     this.pageSet.pageUpdated(this);
   }
 }
--- src/responsive-virtual-list/tests.js
+++ src/responsive-virtual-list/tests.js
@@ -1,5 +1,6 @@
+import {Page, PageSet, Viewport} from './responsive-virtual-list.js';
+
 import test from 'ava';
-import {Viewport, PageSet, Page} from './responsive-virtual-list.js';
 
 test('basic init', t => {
   const pages = [
@@ -10,13 +11,13 @@ test('basic init', t => {
   ];
   const viewport = new Viewport(500, pages);
   const pageSet = viewport.pageSet;
-  
+
   // Viewport should observe first 3 pages
   [0, 1, 2].forEach((x) => t.true(pageSet.observed.has(x)));
   t.false(pageSet.observed.has(3));
 
   t.is(pageSet.estimatedPageHeight(), 200);
-  
+
   // The heightmap should calculate last page as average of first 3: 200.
   t.is(pageSet.heightMap[3], 200);
   t.is(pageSet.totalHeight, 800);
@@ -39,17 +40,6 @@ test('invalidating estimate when observing page height', t => {
   t.is(pageSet.totalHeight, 1600);
 });
 
-test.skip('load from anchor', t => {
-  const pages = [
-    new Page(200),
-    new Page(200),
-    new Page(200),
-    new Page(1000),
-  ];
-  const viewport = new Viewport(500, pages, 3);
-  const pageSet = viewport.pageSet;
-});
-
 test('page position', t => {
   const pages = [
     new Page(200),
@@ -57,13 +47,51 @@ test('page position', t => {
     new Page(200),
     new Page(1000),
   ];
+  const viewport = new Viewport(500, pages);
+  const pageSet = viewport.pageSet;
+
+  [0, 200, 400, 600].forEach((x, i) => t.is(pageSet.positionOfPageTop(i), x));
+});
+
+test('page position starting near end', t => {
+  const pages = [
+    new Page(200),
+    new Page(200),
+    new Page(200),
+    new Page(1000),
+  ];
   const viewport = new Viewport(500, pages, 3);
   const pageSet = viewport.pageSet;
 
-  [0, 200, 400, 600].forEach((x, i) => t.is(pageSet.positionOf(i), x));
+  [0, 1000, 2000, 3000].forEach((x, i) => t.is(pageSet.positionOfPageTop(i), x));
 });
 
-test('jump page position', t => {
+// test('visible', t => {
+//   const pages = [
+//     new Page(200),
+//     new Page(200),
+//     new Page(200),
+//     new Page(1000),
+//   ];
+//   const viewport = new Viewport(500, pages);
+//   const pageSet = viewport.pageSet;
+
+//   t.deepEqual(pageSet.visible(), pages.slice(0, 3));
+
+//   viewport.scrollToPosition(50);
+//   t.deepEqual(pageSet.visible(), pages.slice(0, 3));
+
+//   viewport.scrollToPosition(199);
+//   t.deepEqual(pageSet.visible(), pages.slice(0, 4));
+
+//   viewport.scrollToPosition(200);
+//   t.deepEqual(pageSet.visible(), pages.slice(1, 4));
+
+//   viewport.scrollToPosition(299);
+//   t.deepEqual(pageSet.visible(), pages.slice(1, 4));
+// });
+
+test('page scroll', t => {
   const pages = [
     new Page(200),
     new Page(200),
@@ -73,11 +101,18 @@ test('jump page position', t => {
   const viewport = new Viewport(500, pages);
   const pageSet = viewport.pageSet;
 
-  viewport.jumpIndex(3);
-  t.is(viewport.scrollTop, pageSet.positionOf(3));
+  t.is(pageSet.heightMap[3], 200);
+
+  // Did not scroll enough
+  viewport.scrollDelta(100);
+  t.is(pageSet.heightMap[3], 200);
+
+  // Now scrolled enough.
+  viewport.scrollDelta(100);
+  t.is(pageSet.heightMap[3], 1000);
 });
 
-test('page at position', t => {
+test('page jump', t => {
   const pages = [
     new Page(200),
     new Page(200),
@@ -86,65 +121,126 @@ test('page at position', t => {
   ];
   const viewport = new Viewport(500, pages);
   const pageSet = viewport.pageSet;
+  viewport.scrollDelta(200);
+  // Now all heightmaps should be updated and scroll should be at start of
+  // second page.
+  t.deepEqual(pageSet.heightMap, [200, 200, 200, 1000]);
+  t.is(viewport.top(), 200);
+  t.deepEqual(pageSet.visible(), pages.slice(1));
 
-  const testCases = [
-    {
-      position: 0,
-      index: 0,
-    },
-    {
-      position: 100.5,
-      index: 0,
-    },
-    {
-      position: 199,
-      index: 0,
-    },
-    {
-      position: 200,
-      index: 1,
-    },
-    {
-      position: 599,
-      index: 2,
-    },
-    {
-      position: 600,
-      index: 3,
-    },
-    // TODO: handle out of bounds by pushing in bounds
-    // {
-    //   position: 1600,
-    //   index: 3,
-    // },
-  ];
-  testCases.forEach(({position, index}) => t.is(pageSet.pageAtPosition(position).index, index));
+  // Now, update the height of the first page.
+  pages[0].updateHeight(500);
 
-  const errorCases = [-5, -0.01, 1600.1, 3492309];
-  errorCases.forEach((position) => t.throws(() => pageSet.pageAtPosition(position)));
+  // Now everything should be shifted down.
+  t.deepEqual(pageSet.heightMap, [500, 200, 200, 1000]);
+  t.is(viewport.top(), 500);
+  t.deepEqual(pageSet.visible(), pages.slice(1));
 });
 
-test('visible', t => {
-  const pages = [
-    new Page(200),
-    new Page(200),
-    new Page(200),
-    new Page(1000),
-  ];
-  const viewport = new Viewport(500, pages);
-  const pageSet = viewport.pageSet;
+// test('jump page position', t => {
+//   const pages = [
+//     new Page(200),
+//     new Page(200),
+//     new Page(200),
+//     new Page(1000),
+//   ];
+//   const viewport = new Viewport(500, pages);
+//   const pageSet = viewport.pageSet;
 
-  t.deepEqual(pageSet.visible(), pages.slice(0, 3));
+//   viewport.scrollToTopOfPage(3);
+//   t.is(viewport.scrollTop, pageSet.positionOfPageTop(3));
+// });
 
-  viewport.scrollTop = 50;
-  t.deepEqual(pageSet.visible(), pages.slice(0, 3));
-  
-  viewport.scrollTop = 199;
-  t.deepEqual(pageSet.visible(), pages.slice(0, 4));
-  
-  viewport.scrollTop = 200;
-  t.deepEqual(pageSet.visible(), pages.slice(1, 4));
-  
-  viewport.scrollTop = 299;
-  t.deepEqual(pageSet.visible(), pages.slice(1, 4));
-});
+// test('page at position', t => {
+//   const pages = [
+//     new Page(200),
+//     new Page(200),
+//     new Page(200),
+//     new Page(200),
+//   ];
+//   const viewport = new Viewport(500, pages);
+//   const pageSet = viewport.pageSet;
+
+//   const testCases = [
+//     {
+//       position: 0,
+//       index: 0,
+//     },
+//     {
+//       position: 100.5,
+//       index: 0,
+//     },
+//     {
+//       position: 199,
+//       index: 0,
+//     },
+//     {
+//       position: 200,
+//       index: 1,
+//     },
+//     {
+//       position: 599,
+//       index: 2,
+//     },
+//     {
+//       position: 600,
+//       index: 3,
+//     },
+//     // TODO: handle out of bounds by pushing in bounds
+//     // {
+//     //   position: 1600,
+//     //   index: 3,
+//     // },
+//   ];
+//   testCases.forEach(({position, index}) => t.is(pageSet.pageAtPosition(position).index, index));
+
+//   const errorCases = [-5, -0.01, 1600.1, 3492309];
+//   errorCases.forEach((position) => t.throws(() => pageSet.pageAtPosition(position)));
+// });
+
+// test('visible', t => {
+//   const pages = [
+//     new Page(200),
+//     new Page(200),
+//     new Page(200),
+//     new Page(1000),
+//   ];
+//   const viewport = new Viewport(500, pages);
+//   const pageSet = viewport.pageSet;
+
+//   t.deepEqual(pageSet.visible(), pages.slice(0, 3));
+
+//   viewport.scrollToPosition(50);
+//   t.deepEqual(pageSet.visible(), pages.slice(0, 3));
+
+//   viewport.scrollToPosition(199);
+//   t.deepEqual(pageSet.visible(), pages.slice(0, 4));
+
+//   viewport.scrollToPosition(200);
+//   t.deepEqual(pageSet.visible(), pages.slice(1, 4));
+
+//   viewport.scrollToPosition(299);
+//   t.deepEqual(pageSet.visible(), pages.slice(1, 4));
+// });
+
+// // test('not jumpy going from bottom up', t => {
+// //   const pages = [
+// //     new Page(200),
+// //     new Page(300),
+// //     new Page(400),
+// //     new Page(500),
+// //   ];
+// //   const viewport = new Viewport(500, pages);
+// //   const pageSet = viewport.pageSet;
+
+// //   t.is(pages[3].height, 250); // the page should be the average height of the observed
+// //   t.is(pageSet.totalHeight, 1000); // expect the page to be 4 x 250 tall.
+
+// //   viewport.scrollToPercent(1); // scroll to the bottom of the page
+// //   t.is(pages[3].height, 500); // the page should now be 500 tall.
+// //   t.is(viewport.scrollPercent(), 1); // the scroll bar is still at the bottom
+
+// //   pages[4].updateHeight(1000); // update the page to be taller.
+// //   t.is(viewport.scrollPercent(), 1); // the scroll bar stays at the bottom.
+// //   t.is(pageSet.totalHeight, 1500);
+// // });
